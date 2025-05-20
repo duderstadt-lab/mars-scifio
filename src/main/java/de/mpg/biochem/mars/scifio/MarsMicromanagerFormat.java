7a7611342cdd7ccc51a40203fa8c2f3f5d10cc80
karlduderstadt <duderstadt@biochem.mpg.de>
2025-05-20 13:05:03 +0200

    Update license blurbs

--- src/main/java/de/mpg/biochem/mars/scifio/MarsMicromanagerFormat.java
+++ src/main/java/de/mpg/biochem/mars/scifio/MarsMicromanagerFormat.java
@@ -2,7 +2,7 @@
  * #%L
  * SCIFIO adapted Mars Micromanager format and translator.
  * %%
- * Copyright (C) 2020 - 2023 Karl Duderstadt
+ * Copyright (C) 2020 - 2025 Karl Duderstadt
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
